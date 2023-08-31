1f2cb995c93b911ad465964ef0fe73420f1ad275
felipe arce <arceprogramando@gmail.com>
2023-08-31 11:39:47 -0300

    ☠️ Error :  Need for bug fixes and question time in handling authentications

--- src/middleware/auth.products.js
+++ src/middleware/auth.products.js
@@ -2,12 +2,12 @@ export const isUser = (req, res, next) => {
   if (req.user && req.user.role === 'USER') {
     return next();
   }
-  return res.redirect('/');
+  return res.status(200);
 };
 
 export const isAdmin = (req, res, next) => {
   if (req.user && req.user.role === 'ADMIN') {
-    return next();
+    return res.status(200).redirect('/products');
   }
   return res.redirect('/');
 };
