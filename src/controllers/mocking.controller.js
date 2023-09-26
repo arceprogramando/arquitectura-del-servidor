36f87a0adb3c211952111f312dc7dd6ba14fd379
felipe arce <arceprogramando@gmail.com>
2023-09-26 16:38:44 -0300

    🚀 Added: Added HTTP responses for /mockinproducts and /api/product  with http-status-codes

--- src/controllers/mocking.controller.js
+++ src/controllers/mocking.controller.js
@@ -1,16 +1,19 @@
 import GenerateFakerService from '../services/mocking.services.js';
+import Responses from '../middleware/error.handlers.js';
 
 class GenerateFakerController {
   constructor() {
     this.generateFakerService = new GenerateFakerService();
+    this.httpResponse = new Responses.HttpResponse();
+
   }
 
   generateFakerProducts = async (req, res) => {
     try {
       const generateProduct = await this.generateFakerService.generateFakerProducts();
-      return res.status(200).json(generateProduct);
+      return this.httpResponse.OK(res, 'Generando productos con faker', { generateProducts: generateProduct });
     } catch (error) {
-      return res.status(500).json({ status: 'error', error: `Hubo un problema interno en el controlador al crear el producto. ${error}` });
+      return this.httpResponse.ERROR(res, 'No se han podido crear los productos con faker ', { dataerror: error });
     }
   };
 }
