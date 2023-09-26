7dac045700507c845329394c7d3d9163f8aade7a
felipe arce <arceprogramando@gmail.com>
2023-09-26 18:16:12 -0300

    🔧 Fix :enumerror

--- src/middleware/error.handlers.js
+++ src/middleware/error.handlers.js
@@ -1,11 +1,9 @@
 import { StatusCodes } from 'http-status-codes';
 
 const EnumError = {
-  ROUTING_ERROR: 'ROUTING ERROR',
-  INVALID_TYPES_ERROR: 'INVALID_TYPES_ERROR',
-  CONTROLER_ERROR: 'CONTROLER_ERROR',
-  DB_ERROR: 'DB_ERROR',
-  INVALID_PARAMS: 'INVALID_PARAMS',
+  CONTROLER_ERROR: 'CONTROLER_ERROR: ',
+  INVALID_PARAMS: 'INVALID_PARAMS: ',
+  DB_ERROR: 'DB_ERROR: ',
 };
 
 class HttpResponse {
@@ -33,25 +31,9 @@ class HttpResponse {
     });
   }
 
-  Unauthorized(res, message, data) {
-    return res.status(StatusCodes.UNAUTHORIZED).json({
-      status: StatusCodes.UNAUTHORIZED,
-      statusMessage: message,
-      data,
-    });
-  }
-
   NOT_FOUND(res, message, data) {
     return res.status(StatusCodes.NOT_FOUND).json({
       status: StatusCodes.NOT_FOUND,
-      statusMessage: message,
-      data,
-    });
-  }
-
-  Forbidden(res, message, data) {
-    return res.status(StatusCodes.FORBIDDEN).json({
-      status: StatusCodes.FORBIDDEN,
       statusMessage: message,
       data,
     });
