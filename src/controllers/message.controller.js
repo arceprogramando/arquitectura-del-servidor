ad8ea710982d881e57c63896076dff167fe65f8b
felipe arce <arceprogramando@gmail.com>
2023-09-26 18:29:19 -0300

    🛠️ Apply : 'enumerror' in api/chat and /mockingproducts

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -5,15 +5,19 @@ class messageController {
   constructor() {
     this.messageServices = new MessageServices();
     this.httpResponse = new Responses.HttpResponse();
-
+    this.enumError = Responses.EnumError;
   }
 
   createMessage = async (req, res) => {
     try {
       const { user, message } = req.body;
-      if (!user || !message) {
-        return this.httpResponse.BAD_REQUEST(res, 'Uno de los campos, user o messsage, no se envió correctamente');
+      if (!user) {
+        return this.httpResponse.BAD_REQUEST(res, `${this.enumError.INVALID_PARAMS} El campo "user" es obligatorio`);
       }
+      if (!message) {
+        return this.httpResponse.BAD_REQUEST(res, `${this.enumError.INVALID_PARAMS} El campo "message" es obligatorio`);
+      }
+
       const newMessage = {
         user,
         message,
@@ -22,7 +26,7 @@ class messageController {
       const createdMessage = await this.messageServices.createMessage(newMessage);
       return this.httpResponse.CREATED(res, 'Mensaje Creado', { data: createdMessage });
     } catch (error) {
-      return this.httpResponse.ERROR(res, 'No se ha podido crear el mensaje', { dataerror: error });
+      return this.httpResponse.ERROR(res, `${this.enumError.CONTROLER_ERROR} No se ha podido crear el mensaje`, { dataerror: error });
     }
   };
 }
--- src/controllers/mocking.controller.js
+++ src/controllers/mocking.controller.js
@@ -5,7 +5,7 @@ class GenerateFakerController {
   constructor() {
     this.generateFakerService = new GenerateFakerService();
     this.httpResponse = new Responses.HttpResponse();
-
+    this.enumError = Responses.EnumError;
   }
 
   generateFakerProducts = async (req, res) => {
@@ -13,7 +13,7 @@ class GenerateFakerController {
       const generateProduct = await this.generateFakerService.generateFakerProducts();
       return this.httpResponse.OK(res, 'Generando productos con faker', { generateProducts: generateProduct });
     } catch (error) {
-      return this.httpResponse.ERROR(res, 'No se han podido crear los productos con faker ', { dataerror: error });
+      return this.httpResponse.ERROR(res, `${this.enumError.CONTROLER_ERROR}No se han podido crear los productos con faker `, { dataerror: error });
     }
   };
 }
