660334cac715e2d3da65f7f28dfe07db65c5554c
felipe arce <arceprogramando@gmail.com>
2023-09-26 16:58:41 -0300

    🚀 Added: Added HTTP responses for /api/user

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,19 +1,20 @@
 import encrypt from '../helpers/encrypt.js';
 import UserService from '../services/user.services.js';
+import Responses from '../middleware/error.handlers.js';
 
 class UserController {
   constructor() {
     this.userService = new UserService();
+    this.httpResponse = new Responses.HttpResponse();
+
   }
 
   logoutUser = async (req, res) => {
     try {
       req.session.destroy();
-      res.redirect('/');
-
+      return res.redirect('/');
     } catch (error) {
-      console.error('Error al deslogear:', error);
-      res.status(400).send({ status: 'error', error: `Error al deslogear: ${error}` });
+      return this.httpResponse.ERROR(res, 'error al crear el carrito', { error: error.message });
     }
   };
 
@@ -22,19 +23,22 @@ class UserController {
       const { email, newpassword } = req.body;
       const newPasswordHashed = await encrypt.createHash(newpassword);
       const findUser = await this.userService.findUserByEmail(email);
+
       if (!findUser) {
-        throw new Error('Usuario no encontrado');
+        return this.httpResponse.NOT_FOUND(res, `El usuario con EMAIL: ${email} no fue encontrado`);
+
       }
 
       const updateUser = await this.userService.changePassword(findUser, newPasswordHashed);
-      console.log('updateuser', updateUser);
+
       if (!updateUser) {
-        throw new Error('Problemas al actualizar la contraseña');
+        return this.httpResponse.ERROR(res, 'error al actualizar el carrito el carrito');
+
       }
 
       return res.redirect('/');
     } catch (error) {
-      return res.status(500).json({ message: `Error al resetar la contraseña ${error}` });
+      return this.httpResponse.ERROR(res, 'error al resetear la contraseña');
     }
   };
 }
