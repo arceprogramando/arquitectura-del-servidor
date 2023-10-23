7acd3a2bdc3fb2d55d83da7ca3794635537a51d0
felipe arce <arceprogramando@gmail.com>
2023-10-23 01:41:08 -0300

    🌈 Change : separate session to user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,25 +1,12 @@
 import { Router } from 'express';
-import passport from 'passport';
 import UserController from '../controllers/user.controllers.js';
 
 const router = Router();
 
 const userController = new UserController();
 
-router.post('/register', passport.authenticate('local-register', { failureRedirect: '/register', successRedirect: '/' }));
-
-router.post('/login', passport.authenticate('local-login', { failureRedirect: '/', successRedirect: '/profile' }));
-
-router.get('/github', passport.authenticate('github', { scope: ['user:email'] }));
-
-router.get('/github/callback', passport.authenticate('github', { failureRedirect: '/login', successRedirect: '/products' }));
-
-router.get('/logout', userController.logoutUser);
-
-router.post('/recover-psw', userController.resetPassword);
-
-router.post('/recoverwithemail', userController.recoverWithEmail);
-
 router.get('/premium/:uid', userController.changeRoleWithId);
+
+// router.post('/:uid/documents`, userController.');
 
 export default router;
